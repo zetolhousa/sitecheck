c578f86e83ae874fad4e31083120572d2f907e73
Thejazeto <aron4432@gmail.com>
2014-08-25 21:51:22 +0530

    Initial commit

---
+++ sitecheck.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import argparse
+import requests
+
+def noblank(f):
+    for l in f:
+        line = l.rstrip()
+        if line:
+            yield line
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("urlfile", help="The file containing urls")
+    args = parser.parse_args()
+    urlist = args.urlfile
+
+    with open(urlist) as f:
+        for line in noblank(f):
+            try:
+                req = requests.get(line)
+                resp = req.status_code
+                if resp == 200:
+                    print(line, " >> OK")
+                else:
+                    print(line, " >> Error occured")
+            except requests.exceptions.MissingSchema:
+                print(line, " >> The url format is incorrect")
+
+if __name__ == '__main__':
+    main()              
